0ab71ddffe86b5a41509baa4cca43acde9608498
Aditya Bisai <adityabisai1234@gmail.co>
2026-03-14 21:19:47 +0530

    done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,21 +5,14 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: function (origin, callback) {
-        const allowed = [
-            "http://localhost:5173",
-            "https://interviewforge-ai-1.onrender.com"
-        ]
-        // Allow any vercel preview deployment of your app
-        if (!origin || allowed.includes(origin) || /https:\/\/ai-interview-prep-fontend-n9yt.*\.vercel\.app/.test(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error("Not allowed by CORS"))
-        }
-    },
-    credentials: true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://ai-interview-prep-fontend.vercel.app"
+  ],
+  credentials: true
+}));
 
 /* require all the routes */
 const authRouter = require("./routes/auth.routes")
